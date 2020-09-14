492c3d5c46c22265d7c72b626cf89aea02d23725
Andrii Avramuk <avramuk.andrii@gmail.com>
2020-09-14 20:57:10 +0300

    add test for success message

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,9 +15,18 @@ class ProductPage(BasePage):
     def check_product_name_added_to_cart(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_name_in_add_to_cart_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_ADD_TO_CART_MESSAGE).text
-        assert product_name == product_name_in_add_to_cart_message, f'Product name in add message is wrong. Name: {product_name}, Name in cart message: {product_name_in_add_to_cart_message}'
+        assert product_name == product_name_in_add_to_cart_message, \
+            f'Product name in add message is wrong. Name: {product_name}, Name in cart message: {product_name_in_add_to_cart_message}'
 
     def check_price_added_to_cart(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         total_busket = self.browser.find_element(*ProductPageLocators.TOTAL_BUSKET).text
         assert product_price == total_busket, f'Poduct price in add message is wrong. Price: {product_price}, Busket: {total_busket} '
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADD_TO_CART_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_must_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.ADD_TO_CART_MESSAGE), \
+            "Success message is not disappered"
--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,7 @@ from pages.product_page import ProductPage
 import time
 
 
-# link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -24,3 +24,21 @@ def  test_guest_can_add_product_to_cart(browser, link):
     page.check_product_name_added_to_cart()
     page.check_price_added_to_cart()
     
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link, timeout=0)
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link, timeout=0)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link, timeout=0)
+    page.open()
+    page.add_to_cart()
+    page.success_message_must_be_disappeared()
