5654b3c181d1295d7f18f1b85b220382ad94cc85
Andrii Avramuk <avramuk.andrii@gmail.com>
2020-09-14 15:25:16 +0300

    add locators and login_page tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,26 @@
+import pytest
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
+link = "http://selenium1py.pythonanywhere.com/"
 
+# @pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  
     page.open()                      
     page.go_to_login_page()     
+
+
+# @pytest.mark.skip
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+# @pytest.mark.skip
+def test_login_page_should_be_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
